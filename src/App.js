516711abdb970090bc75c626dc9e8f6668ec96a1
crazycoder1729 <crazycoder1729@gmail.com>
2024-01-30 11:01:53 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import Naving from './component/Naving'
+import Home from './component/Home'
 import './App.css';
-
+// scroll animation
+// loop animation
+// horizantal scroll
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Naving />
+      <Home />
     </div>
   );
 }
---
+++ src/component/Naving.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Naving() {
+  return (
+    <div>Naving</div>
+  )
+}
+
+export default Naving
