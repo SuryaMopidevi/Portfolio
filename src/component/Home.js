4202e6cb977879f75b6f4229023b1d5e9333a425
MOPIDEVI PAVITRASURYA <94208004+SuryaMopidevi@users.noreply.github.com>
2024-01-31 08:13:48 +0530

    commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,11 @@
 import React ,{ useState }from 'react'
+import { useEffect } from 'react';
 import './home.css'
 import { Form, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faFacebook, faWhatsapp, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram,faGithub,faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faPhone,faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import Typed from 'typed.js';
 
 function Home() {
     const [selectedFile, setSelectedFile] = useState(null);
@@ -22,19 +25,33 @@ function Home() {
         console.log('No file selected');
       }
     };
+    useEffect(() => {
+      const options = {
+        strings: ['Software Developer','Python Developer','Software Engineer'],
+        typeSpeed: 50, 
+        backSpeed: 25,
+      };
+  
+      const typed = new Typed('.text', options);
+      return () => {
+        typed.destroy();
+      };
+    }, []); 
   return (
     <div className='content'>
       <div>
         <h3> Hello ,it's Me</h3>
         <h1>Mopidevi Surya</h1>
-        <h3>And I'm a Software DEVELOPER</h3>
+        <h3>And I'm a <span className="text"></span></h3>
         <p>A software developer is a professional who specializes in creating, designing, and maintaining software applications. These individuals play a crucial role in the development lifecycle, contributing their expertise to produce high-quality, functional, and efficient software solutions.</p>
       </div>
       <div>
-      <FontAwesomeIcon className='icon' icon={faInstagram} size="2x" style={{ margin: '0 10px' }} />
-      <FontAwesomeIcon className='icon' icon={faFacebook} size="2x" style={{ margin: '0 10px' }} />
-      <FontAwesomeIcon className='icon' icon={faWhatsapp} size="2x" style={{ margin: '0 10px' }} />
-      <FontAwesomeIcon className='icon' icon={faGoogle} size="2x" style={{ margin: '0 10px' }} />
+      <FontAwesomeIcon className='icon' icon={faInstagram} size="5x" style={{ margin: '0 10px' }} />
+      {/* <FontAwesomeIcon className='icon' icon={faFacebook} size="2x" style={{ margin: '0 10px' }} /> */}
+      <FontAwesomeIcon className='icon' icon={faGithub} size="2x" style={{ margin: '0 10px' }} />
+      <FontAwesomeIcon className='icon' icon={faPhone} size="2x" style={{ margin: '0 10px' }} />
+      <FontAwesomeIcon className='icon' icon={faLinkedin} size="2x" style={{ margin: '0 10px' }} />
+      <FontAwesomeIcon className='icon' icon={faEnvelope} size="2x" style={{ margin: '0 10px' }}/>
       <br/><br/><Button className='aboutMe'>More About Me</Button>
     </div>
       <div className='PhotoAdd'>
